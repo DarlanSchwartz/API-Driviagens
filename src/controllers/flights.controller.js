b1d32bec83286cb6b0dc17962def02e5c7434f58
Schwartz <darlanswtz@gmail.com>
2023-08-30 16:56:08 -0300

    Fix: get flights working

--- src/controllers/flights.controller.js
+++ src/controllers/flights.controller.js
@@ -1,19 +1,17 @@
 import dayjs from "dayjs";
 import { getCityId } from "../repository/cities.repository.js";
 import { createFlight, findFlights, validFlightLocations } from "../repository/flights.repository.js";
-import { isValidDateFormat, dateBiggerThan, dateBiggerThanFormatBoth } from "../services/dateServices.js";
+import { isValidDateFormat, dateBiggerThan, dateBiggerThanFormatBoth, formatDate } from "../services/dateServices.js";
 
 export async function registerFlight(req, res) {
     const { origin, destination, date } = req.body;
     if (origin == destination) return res.sendStatus(409);
     if (!dateBiggerThan(date, new Date())) return res.status(422).send('WHO DO YOU THINK YOU ARE?!');
-    const partesData = date.split("-");
-    const formatedReceivedDate = new Date(partesData[2], partesData[1] - 1, partesData[0]);
+   
     try {
         const isValidFlight = await validFlightLocations(origin, destination);
-        if (!isValidFlight) return res.sendStatus(404);
-        
-        await createFlight(origin, destination, formatedReceivedDate);
+        if (!isValidFlight) return res.sendStatus(404); 
+        await createFlight(origin, destination, formatDate(date));
         return res.sendStatus(201);
     }  catch ({message}) {
         console.log(message);
@@ -29,7 +27,7 @@ export async function getFlights(req, res) {
     if (biggerDate && !smallerDate || smallerDate && !biggerDate) return res.sendStatus(422);
     if (smallerDate && !isValidDateFormat(smallerDate)) return res.sendStatus(422);
     if (biggerDate && !isValidDateFormat(biggerDate)) return res.sendStatus(422);
-    if (smallerDate && biggerDate && !dateBiggerThanFormatBoth(smallerDate, biggerDate)) return res.sendStatus(400);
+    if (smallerDate && biggerDate && dateBiggerThanFormatBoth(smallerDate, biggerDate)) return res.sendStatus(400);
 
     try {
         let originId = null;
@@ -40,9 +38,11 @@ export async function getFlights(req, res) {
         if (destination) {
             destinationId = await getCityId(destination);
         }
-        const flights = await findFlights(originId, destinationId, smallerDate ? smallerDate : null, biggerDate ? biggerDate : null);
+        const flights = await findFlights(originId, destinationId, smallerDate ? formatDate(smallerDate) : null, biggerDate ? formatDate(biggerDate) : null);
+        if(origin && destination && flights.length == 0) return res.sendStatus(404);
         return res.status(200).send(flights);
     } catch ({message}) {
+        console.log(message);
         throw { type: "get_flights", message };
     }
 }
--- src/services/dateServices.js
+++ src/services/dateServices.js
@@ -1,21 +1,20 @@
 export function isValidDateFormat(date) {
-    if(!date) return false;
+    if (!date) return false;
     const dateRegex = /^\d{2}-\d{2}-\d{4}$/;
     return dateRegex.test(date);
 }
 
-export function dateBiggerThan(smallerDate,biggerDate) {
-    if(!smallerDate || !biggerDate) return false;
-    const partesData = smallerDate.split("-");
-    const formatedReceivedDate = new Date(partesData[2], partesData[1] - 1, partesData[0]);
-    return formatedReceivedDate > biggerDate;
+export function dateBiggerThan(smallerDate, biggerDate) {
+    if (!smallerDate || !biggerDate) return false;
+    return formatDate(smallerDate) > biggerDate;
 }
 
-export function dateBiggerThanFormatBoth(smallerDate,biggerDate) {
-    if(!smallerDate || !biggerDate) return false;
-    const smallerDateParts = smallerDate.split("-");
-    const biggerDateParts = biggerDate.split("-");
-    const formatedReceivedSmallerDate = new Date(smallerDateParts[2], smallerDateParts[1] - 1, smallerDateParts[0]);
-    const formatedReceivedBiggerDate = new Date(biggerDateParts[2], biggerDateParts[1] - 1, biggerDateParts[0]);
-    return formatedReceivedSmallerDate <= formatedReceivedBiggerDate;
+export function dateBiggerThanFormatBoth(smallerDate, biggerDate) {
+    if (!smallerDate || !biggerDate) return false;
+    return formatDate(smallerDate) > formatDate(biggerDate);
+}
+
+export function formatDate(date) {
+    const dateParts = date.split("-");
+    return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
 }
